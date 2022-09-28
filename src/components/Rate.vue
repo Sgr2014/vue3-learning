96fe6da6d3b75d0d3a0e8fd4166c14bd3c316515
xbhu2014@126.com <xbhu2014@126.com>
2022-09-28 11:15:49 +0800

    feat: add Rate comp.

---
+++ src/components/Rate.vue
@@ -0,0 +1,61 @@
+<template>
+  <div :style="fontStyle">
+    <div class="rate" @mouseout="mouseOut">
+      <span @mouseover="mouseOver(num)" v-for="num in 5" :key="num">☆</span>
+      <span class="hollow" :style="fontWidth">
+        <span @mouseover="mouseOver(num)" v-for="num in 5" :key="num">★</span>
+      </span>
+    </div>
+  </div>
+</template>
+<script setup>
+  import { defineProps, computed, ref } from 'vue'
+  let props = defineProps({
+    value: Number,
+    theme: {
+      type: String,
+      default: 'orange'
+    }
+  })
+  let rate = computed(() => {
+    return "★★★★★☆☆☆☆☆".slice(5 - props.value, 10 - props.value)
+  })
+  const themes = {
+    'black': '#000',
+    'white': '#fff',
+    'red': '#f5222d',
+    'orange': '#fa541c',
+    'yellow': '#fadb14',
+    'green': '#73d13d',
+    'blue': '#40a9ff'
+  }
+  const fontStyle = computed(() => {
+    return `color:${themes[props.theme]}`
+  })
+  const fontWidth = computed(() => {
+    return `width: ${width.value}em`
+  })
+
+  // 评分宽度
+  let width = ref(props.value)
+  function mouseOver (i) {
+    width.value = i
+  }
+  function mouseOut () {
+    width.value = props.value
+  }
+</script>
+<style scoped>
+  .rate{
+    position:relative;
+    display: inline-block;
+  }
+  .rate > span.hollow {
+    position:absolute;
+    display: inline-block;
+    top:0;
+    left:0;
+    width:0;
+    overflow:hidden;
+  }
+</style>
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,3 +1,9 @@
 <template>
   <h1>我是About页面</h1>
+  <Rate :value="score"></Rate>
 </template>
+<script setup>
+  import { ref } from 'vue'
+  import Rate from '../components/Rate.vue'
+  let score = ref(3)
+</script>
