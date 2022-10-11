b73cf593b1918b5b9f82ede388cf9a0757fff527
xbhu2014@126.com <xbhu2014@126.com>
2022-10-11 14:12:35 +0800

    feat: Rate component.

--- src/components/Rate.vue
+++ src/components/Rate.vue
@@ -1,24 +1,35 @@
 <template>
+  <slot></slot>
   <div :style="fontStyle">
     <div class="rate" @mouseout="mouseOut">
       <span @mouseover="mouseOver(num)" v-for="num in 5" :key="num">☆</span>
       <span class="hollow" :style="fontWidth">
-        <span @mouseover="mouseOver(num)" v-for="num in 5" :key="num">★</span>
+        <span @mouseover="mouseOver(num)" v-for="num in 5" :key="num" @click="onRate(num)">★</span>
       </span>
     </div>
   </div>
 </template>
 <script setup>
-  import { defineProps, computed, ref } from 'vue'
+  /**
+   * 问题：
+   * 1、空心rate和实心rate宽度不一致，导致评分出现偏差
+   * 2、父组件通过v-model传入的是ref对象，为什么props的modelValue不需要通过.value来获取值呢
+   */
+  import { defineProps, defineEmits, computed, ref } from 'vue'
   let props = defineProps({
-    value: Number,
+    modelValue: Number,
     theme: {
       type: String,
       default: 'orange'
     }
   })
+  let emits = defineEmits(['update:modelValue'])
+  function onRate (num) {
+    emits('update:modelValue', num)
+  }
+  // 很巧妙
   let rate = computed(() => {
-    return "★★★★★☆☆☆☆☆".slice(5 - props.value, 10 - props.value)
+    return "★★★★★☆☆☆☆☆".slice(5 - props.modelValue, 10 - props.modelValue)
   })
   const themes = {
     'black': '#000',
@@ -37,12 +48,12 @@
   })
 
   // 评分宽度
-  let width = ref(props.value)
+  let width = ref(props.modelValue)
   function mouseOver (i) {
     width.value = i
   }
   function mouseOut () {
-    width.value = props.value
+    width.value = props.modelValue
   }
 </script>
 <style scoped>
@@ -50,12 +61,16 @@
     position:relative;
     display: inline-block;
   }
+  .rate .empty {
+    display: inline-block;
+    width: 1em;
+  }
   .rate > span.hollow {
-    position:absolute;
+    position: absolute;
     display: inline-block;
     top:0;
     left:0;
-    width:0;
+    width: 0;
     overflow:hidden;
   }
 </style>
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,9 +1,14 @@
 <template>
   <h1>我是About页面</h1>
-  <Rate :value="score"></Rate>
+  <Rate v-model="score">
+    <h2>评分</h2>
+  </Rate>
 </template>
 <script setup>
-  import { ref } from 'vue'
+  import { ref, watch } from 'vue'
   import Rate from '../components/Rate.vue'
-  let score = ref(3)
+  let score = ref(3.5)
+  watch(score, (val) => {
+    console.log(val, 'score.value...')
+  })
 </script>
